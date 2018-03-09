3aecd0ed23a5ecb36a0641b453639d9518208528
Cornelius Jemison <cornelius.jemison@nielsen.com>
2018-03-09 12:56:35 -0600

    Cleanup the

---
+++ src/main/java/com/nielsen/ndx/unzip/paas_unzip_app/PaasUnzipAppApplication.java
@@ -0,0 +1,12 @@
+package com.nielsen.ndx.unzip.paas_unzip_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaasUnzipAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaasUnzipAppApplication.class, args);
+  }
+}
